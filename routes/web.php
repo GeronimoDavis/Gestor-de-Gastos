d8542607d1048349868cada08669233c3943e8dc
GeronimoDavis <geronimodavis@gmail.com>
2026-08-14 23:38:24 -0300

    Primeras rutas para usuarios autenticados y no autenticados

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+//rutas accesibles solo para usuarios no autenticados
+
+Route::middleware('gust')->group(function (){
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('register')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+});
+
+//Rutas protegidas solo para usuarios autenticados
+
+Route::middleware('auth')->group(function() {
+    Route::get('/dashboard', function(){
+        return view('dashboard');
+    })->name('dashboard');
+
+   Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
