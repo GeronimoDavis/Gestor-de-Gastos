4e81c03a9877cc36cce19605bb7db61fda2b3c9d
GeronimoDavis <geronimodavis@gmail.com>
2026-08-15 23:45:17 -0300

    Controlador de registro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    
+    // Mostrar formulario de registro
+    public function showRegister(){
+        return view('auth.register');
+    }
+
+    // Procesar el registro de usuario
+    public function register(Request $request){
+
+        $credentials = $request->validate([
+            'name' => 'required|string|max255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $credentials['name'],
+            'email' => $credentials['email'],
+            'password' => Hash::make($credentials['password']),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
+    }
 }
