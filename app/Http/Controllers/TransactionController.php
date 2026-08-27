4947753dc57e07dadfe3996e2eb6e04340470ee3
GeronimoDavis <geronimodavis@gmail.com>
2026-08-26 23:58:17 -0300

    Crear y borrar de transacciones

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\Transaction;
 
 class TransactionController extends Controller
 {
     public function index(Request $request){
-        //Obtenemos las transacciones del usuario logueado, cargando su categoría
+        //Obtenemos las transacciones del usuario logueado, cargando su categoría buscando en el modelos de transacctions 
+        //el metodo llamado exactamente 'category' con with
         $transactions = $request->user()
             ->transactions()
             ->with('category')
@@ -18,5 +20,30 @@ class TransactionController extends Controller
         $categories = $request->user()->categories()->get();
 
         return view('transactions.index', compact('transactions', 'categories'));
+    }
+
+    public function store(Request $request){
+        
+        $validated = $request->validate([
+            'category_id' => 'required|exists:categories,id',
+            'amount' => 'required|numeric|gt:0',
+            'transaction_date' => 'required|date',
+            'description' => 'nullable|string|max:250',
+            'type' => 'required|in:expense,income',
+    ]);
+
+        $request->user()->transactions()->create($validated);
+
+        return back()->with('success', 'Transaccion creada con éxito.');
+    }
+
+    public function destroy(Request $request, Transaction $transaction){
+        if($transaction->user_id !== $request->user()->id){
+            abort(403);
+        }
+
+        $transaction->delete();
+
+        return back()->with('success', 'Transacción eliminada.');
     }
 }
