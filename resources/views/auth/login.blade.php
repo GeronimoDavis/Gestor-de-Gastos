d7c4badf7016ca8be5ff95f010c9fcb297bfa7a4
Geronimo Davis <geronimodavis@gmail.com>
2026-09-25 16:24:05 -0300

    Estilos de transacciones terminado y login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Iniciar Sesión - Gestor de Gastos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-    <h2>Iniciar Sesión</h2>
-
     @if ($errors->any())
         <ul style="color: red;">
             @foreach ($errors->all() as $error)
@@ -15,26 +15,32 @@
         </ul>
     @endif
 
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <div>
-            <label>Email:</label><br>
-            <input type="email" name="email" value="{{ old('email') }}" required>
-        </div>
-        <br>
-        <div>
-            <label>Contraseña:</label><br>
-            <input type="password" name="password" required>
-        </div>
-        <br>
-        <div>
-            <label>
-                <input type="checkbox" name="remember"> Recordarme
-            </label>
-        </div>
-        <br>
-        <button type="submit">Ingresar</button>
-    </form>
+    <h2 >Iniciar Sesión</h2>
+
+    <section class="login-container">
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="login-form">
+                <div>
+                    <label class="form-label">Email:</label><br>
+                    <input class="form-input" type="email" name="email" value="{{ old('email') }}" required>
+                </div>
+                <br>
+                <div>
+                    <label class="form-label">Contraseña:</label><br>
+                    <input class="form-input" type="password" name="password" required>
+                </div>
+                <br>
+                <div>
+                    <label>
+                        <input class="form-checkbox" type="checkbox" name="remember"> Recordarme
+                    </label>
+                </div>
+                <br>
+                <button class="btn btn-primary" type="submit">Ingresar</button>
+            </div>
+        </form>
+    </section>
     <p>¿No tenés cuenta? <a href="{{ route('register') }}">Registrate acá</a></p>
 
 </body>
